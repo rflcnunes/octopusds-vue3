6876673a5cc2a030ef948e3f897eee4632c2a1df
rafael <rafaelcnunes@icloud.com>
2023-05-19 20:24:58 -0300

    added storybook

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./lib/octopus.js', import.meta.url)), // Arquivo principal da sua biblioteca
+      name: 'Octopus', // Nome da sua biblioteca
+      fileName: (format) => `octopus.${format}.js` // Nome do arquivo de saída
+    },
+    rollupOptions: {
+      // Opções de configuração do Rollup
+      // Aqui você pode configurar plugins adicionais, opções de empacotamento, etc.
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
